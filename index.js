1beebc1792be56f5b1a4f46c5b8bf1130f801bba
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-06-01 04:05:56 +0600

    Context Done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.meus3dj.mongodb.net/?retryWrites=true&w=majority`;
 const app = express();
 const port = process.env.PORT || 5000;
@@ -29,12 +29,28 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const serviceColection = client.db("carDoctor").collection("services");
+        const bookingColection = client.db("carDoctor").collection("bookings");
 
         app.get('/services', async (req, res) => {
             const carsor = serviceColection.find()
             const result = await carsor.toArray()
             res.send(result)
-        })
+        });
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const options = {
+                projection: { title: 1, price: 1, service_id: 1, img: 1 }
+            };
+            const result = await serviceColection.findOne(query, options);
+            res.send(result);
+        });
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingColection.insertOne(booking);
+            res.send(result);
+        });
 
 
 
